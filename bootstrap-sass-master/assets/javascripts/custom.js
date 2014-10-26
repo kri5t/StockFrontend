5214411e23ced9dad30a3a95a3e5e228491b9495
Allan Larsen <alns@alns.eu>
2014-10-26 16:45:28 +0100

    Add custom.js

---
+++ bootstrap-sass-master/assets/javascripts/custom.js
@@ -0,0 +1,19 @@
+/**
+ * Created by allanlarsen on 26/10/14.
+ */
+
+// smooth scroll snippet on #[hashtaglink]
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
