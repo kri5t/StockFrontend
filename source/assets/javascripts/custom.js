fa1a44e17ecd8d01b4c932563d6eada3287957d7
Allan Larsen <alns@alns.eu>
2014-12-31 00:28:46 +0100

    Major changes to stock.html, profile.html in regards to signal cards. Some minor changes to about.html

--- source/assets/javascripts/custom.js
+++ source/assets/javascripts/custom.js
@@ -6,17 +6,15 @@
 $('document').ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 
-//Hide how-to measure and add margin to needs h2
-    $('.read-more').on('click', function() {
-        $(this).parent().hide();
+
+    //Show signal
+    $('.read-more').on('click', function(e) {
+        $(this).text(function(i, text){
+            return text === 'læs mere' ? 'skjul' : 'læs mere';
+        });
     });
 
-//Hide how-to measure and add margin to needs h2
-    $('.read-more.pull-right').on('click', function() {
-        $('.signal-teaser').show();
-    });
-
-//Trigger
+//Follow stock
     $('.follow-stock').on('click', function(e) {
         $(this).text(function(i, text){
             return text === 'Følg aktie' ? 'Følg ikke aktie' : 'Følg aktie';
